ad882f7243ca085f2117b5d6f9673691afd6eb3d
Nith567 <nithinkatkam504106@gmail.com>
2025-09-28 02:51:36 +0530

    export commands

---
+++ src/commands/index.ts
@@ -0,0 +1,21 @@
+// CryptoNomads Verification Commands
+export { 
+  cryptoNomadsVerifyCommand,
+  executeCryptoNomadsVerify,
+  verifyStatusCommand,
+  executeVerifyStatus,
+  checkStatusCommand,
+  executeCheckStatus,
+  setupChannelsCommand,
+  executeSetupChannels,
+  userDetailsCommand,
+  executeUserDetails,
+  resetPermissionsCommand,
+  executeResetPermissions,
+  sendCommand,
+  executeSend,
+  dmPrivateKeyCommand,
+  executeDMPrivateKey,
+  emergencyLockdownCommand,
+  executeEmergencyLockdown
+} from "./verification.js";
